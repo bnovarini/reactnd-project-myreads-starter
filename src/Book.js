27c718447c6aea202b376517b663037edbf3bde1
Bruno Novarini <brunonovarini@Brunos-MacBook-Pro.local>
2020-06-15 10:15:33 -0400

    fix author not showing

--- src/Book.js
+++ src/Book.js
@@ -65,7 +65,10 @@ class Book extends Component {
             </div>
           </div>
           <div className="book-title">{this.props.book.title}</div>
-          <div className="book-authors">{this.props.book.author}</div>
+          <div className="book-authors">
+            {this.props.book.authors.length > 0 &&
+              this.props.book.authors.map((author) => <p>{author}</p>)}
+          </div>
         </div>
       </li>
     );
