9f07ad3da1bfc9273c2f5b701881de7e3fa127b6
Bruno Novarini <brunonovarini@Brunos-MacBook-Pro.local>
2020-06-15 10:18:08 -0400

    fix bug when no author available

--- src/Book.js
+++ src/Book.js
@@ -66,7 +66,7 @@ class Book extends Component {
           </div>
           <div className="book-title">{this.props.book.title}</div>
           <div className="book-authors">
-            {this.props.book.authors.length > 0 &&
+            {this.props.book.authors &&
               this.props.book.authors.map((author) => <p>{author}</p>)}
           </div>
         </div>
